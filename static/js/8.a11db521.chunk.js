87d6ebd7b6c1f20b54c846114eae6162d5b09d1b
Yadurani Lopez Rivera <yadu-lopez@hotmail.com>
2021-07-15 22:24:23 -0500

    Updates

---
+++ static/js/8.a11db521.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpecommerce-prueba"]=this["webpackJsonpecommerce-prueba"]||[]).push([[8],{57:function(e,n,t){"use strict";t.r(n);var c,r=t(3),i=(t(1),t(4)),s=t(2),a=i.b.div(c||(c=Object(r.a)(["\n  align-items: center;\n  display: flex;\n  flex-direction: column;\n  justify-content: center;\n  margin: 40px 0;\n"])));n.default=function(){return Object(s.jsxs)(a,{children:["\ud83d\ude1e\u2639\ud83d\ude1e",Object(s.jsx)("p",{children:"Error, vuelve a intentarlo."})]})}}}]);
+//# sourceMappingURL=8.a11db521.chunk.js.map
